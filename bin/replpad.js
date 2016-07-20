ba013787d9f6806d265b97ca17071d56c2381a48
CarlosLoriaSaenz <CarlosLoriaSaenz@users.noreply.github.com>
2016-07-19 18:57:25 -0600

    Add files via upload

--- bin/replpad.js
+++ bin/replpad.js
@@ -2,7 +2,15 @@
 
 'use strict';
 var repreprep = require('..')
-  , path = require('path');
+  , path = require('path')
+  , log = require('../lib/log')
+  , fs = require('fs');
+  
+var dirToWatch = process.argv[2];
+if (dirToWatch && (!fs.existsSync(dirToWatch) || !fs.lstatSync(dirToWatch).isDirectory())){
+	log.error("'" + dirToWatch + "'" +  ' is invalid. Provide an existing directory, please');
+	process.exit();
+}
 
 var root = process.argv.length > 2 ? path.resolve(process.argv[2]) : null;
 
